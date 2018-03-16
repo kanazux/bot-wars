7e86e10faa9ae8d51fb09f6d87311a00ea23815d
kanazux <alvolivre@live.com>
2018-03-16 10:13:18 -0300

    Added bot_wars

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ from setuptools import setup
 setup(name="bot_wars",
       version="0.1",
       packages=["bot_wars"],
+      license="BSD2CLAUSE",
+      scripts=['scripts/bot_wars.py'],
+      url="https://github.com/kanazux/bot-wars",
+      author_email="contato@kanazuchi.com",
+      description="A simple telegram bot to get mob info.",
+      install_requires=['bs4', 'requests', 'requests-html'],
       zip_safe=False)
